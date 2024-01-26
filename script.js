462f381a26c800f886d90c20dc1dce24e2218122
ali <mustafaevalibek52@gmail.com>
2024-01-26 16:50:16 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let img = document.querySelector(`img`)
+
+
+let button = document.querySelector(`#odin`)
+let dblbutton = document.querySelector(`#dwa`)
+
+button.onclick = () => {
+    img.src = `img/mbp14-silver-select-202110 1.png`
+
+}
+dblbutton.onclick = () => {
+    img.src = `img/mbp14-spacegray-select-202110 1.png`
+}
